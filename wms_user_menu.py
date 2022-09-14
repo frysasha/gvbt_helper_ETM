bed874ed8d74eca6ce37c0f699d04f713d2bc724
frysasha <93375902+frysasha@users.noreply.github.com>
2022-09-14 12:16:24 +0300

    подготовка к переносу

--- wms_user_menu.py
+++ wms_user_menu.py
@@ -38,11 +38,17 @@ wms_user_list_query = None
 def wms_user_list_menu(update, context):
     quit_browser_driver()
     query = update.callback_query
-    query.answer()
+    context.bot.answer_callback_query(update.callback_query.id)
+    #query.answer()
     global wms_user_list_query
     wms_user_list_query = query
     query.edit_message_text(text="Загрузка...")
-    user_list = get_user_list()
+    try:
+        user_list = get_user_list()
+    except:
+        query.edit_message_text(text="Попробуйте позже")
+        quit_browser_driver()
+        return ConversationHandler.END
     if user_list == 'Нет активных пользователей ТСД':
         query.edit_message_text(text=user_list)
         quit_browser_driver()
@@ -52,7 +58,6 @@ def wms_user_list_menu(update, context):
     ]
     menu_keyboard = InlineKeyboardMarkup(build_menu(wms_usr_menu_list, n_cols=1))
     query.edit_message_text(text=user_list, reply_markup=menu_keyboard)
-    #query.edit_message_text(text="Введи номер пользователя")
     return WMS_USER_LIST_MENU
 
 
@@ -60,7 +65,8 @@ def are_you_sure_menu(update, context):
 
     if update.callback_query:
         query = update.callback_query
-        query.answer(cache_time=5)
+        context.bot.answer_callback_query(update.callback_query.id)
+        #query.answer()
         wms_user_name = wms_user_dict[update.callback_query.data]
         del_user_from_wms(wms_user_name)
         query.edit_message_text(text=f'{wms_user_name} Удален', reply_markup='')
@@ -87,9 +93,10 @@ def wrong_number(update, context):
     return WMS_USER_LIST_MENU
 
 
-def exit_from_wms_menu(update, _):
+def exit_from_wms_menu(update, context):
     query = update.callback_query
-    query.answer()
+    context.bot.answer_callback_query(update.callback_query.id)
+    #query.answer()
     query.edit_message_text(text='Выход', reply_markup='')
     quit_browser_driver()
     return ConversationHandler.END
